f3fd575dea26df19ff62604612e2dd2a1aad75b2
Giulia Naponiello <gnaponie@redhat.com>
2020-02-02 17:29:18 +0100

    A simple API with some error handling
    Signed-off-by: Giulia Naponiello <gnaponie@redhat.com>

--- hello.py
+++ hello.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, request
+from werkzeug.exceptions import MethodNotAllowed, BadRequest
 app = Flask(__name__)
 
 
-@app.route('/')
+def sum(a, b):
+    return a + b
+
+
+@app.route('/', methods=['GET'])
 def hello_world():
-    return 'hello, world!'
+    if request.method != 'GET':
+        return MethodNotAllowed('Only GET method is allowed')
+
+    if 'a' not in request.args or 'b' not in request.args:
+        return BadRequest('Parameters \'a\' and \'b\' are mandatory')
+    return sum(request.args['a'], request.args['b'])
